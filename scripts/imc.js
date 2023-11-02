601b61d7c58c2d8979d219495de76979b0ff86c9
Talyslan Canabarro <78499700+Talyslan@users.noreply.github.com>
2023-11-01 21:32:18 -0300

    finalizado por enquanto

---
+++ scripts/imc.js
@@ -0,0 +1,31 @@
+const btnCalculate = document.querySelector("#calculate");
+const result = document.querySelector("#result");
+// ranges
+let age = document.querySelector("#age");
+let height = document.querySelector("#height");
+let weight = document.querySelector("#weight");
+
+const atualizeValues = () => {
+    age = parseInt(document.querySelector("#age").value);
+    height = parseInt(document.querySelector("#height").value);
+    weight = parseInt(document.querySelector("#weight").value);
+}
+// changes values changes interface
+const heightChange = document.querySelector("#heightChange");
+const weightChange = document.querySelector("#weightChange");
+const ageChange = document.querySelector("#ageChange");
+
+age.addEventListener("input", () => {atualizeValues(); ageChange.innerHTML = age})
+height.addEventListener("input", () => {atualizeValues();heightChange.innerHTML = height})
+weight.addEventListener("input", () => {atualizeValues();weightChange.innerHTML = weight})
+
+const calculate = () => {
+    atualizeValues();
+    height = height / 100;
+    
+    let imc = (weight / (height * height)).toFixed(2);
+    
+    result.innerHTML = imc;
+};
+
+btnCalculate.addEventListener("click", calculate);
